b2ea33116e628a010d7beef974c44b64b2ad9982
amirmadethis <aa.valipour@hotmail.com>
2021-10-19 00:53:39 -0700

    New lab

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+// Summary. car info.
+// Description. This file carries the information of a vehicle.
+// @link   x
+// @file   x
+// @author Amirarsalan.
+// @since  10.18.2021
+
+// Define variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year  = 1995;
+
+
+// Calculate
+age = 2021 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
